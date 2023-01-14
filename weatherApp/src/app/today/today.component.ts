87269f7a65109da69f9b5669b4f6625b71045494
Mansi Sonawani <12345mansis@gmail.com>
2023-01-14 19:11:56 +0530

    geolocation

--- weatherApp/src/app/today/today.component.ts
+++ weatherApp/src/app/today/today.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { getLocaleDirection } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-today',
   templateUrl: './today.component.html',
   styleUrls: ['./today.component.css']
 })
-export class TodayComponent {
+export class TodayComponent implements OnInit {
+  lat : number | undefined;
+  lon: number | undefined;
+  weather: Object | undefined;
 
+  constructor(private weatherService : WeatherService){}
+   
+  ngOnInit()  {
+      this.getLocation()
+
+}
+getLocation(){
+  if("geolocation" in navigator){
+    navigator.geolocation.watchPosition((success) => {
+      this.lat = success.coords.latitude;
+      this.lon = success.coords.longitude;
+
+      this.weatherService.getWeatherDataByCoords(this.lat, this.lon).subscribe(data =>{
+        this.weather = data;
+      });
+
+
+    })
+    
+  }
+}
 }
--- weatherApp/src/app/weather.service.ts
+++ weatherApp/src/app/weather.service.ts
@@ -1,15 +1,22 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-url = 'https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid={459d7ab5fab85c9b3ce557236603f3eb}';
-  constructor(private http: HttpClient) { }
+url = 'https://api.openweathermap.org/data/2.5/weather';
+apiKey = '459d7ab5fab85c9b3ce557236603f3eb';
+   constructor(private http: HttpClient) { }
 
-}
 
-getWeatherDataByCoords(lat,lon){
+getWeatherDataByCoords(lat: number ,lon:  number ){
   
+  let params = new HttpParams()
+  .set('lat', lat)
+  .set('lon', lon)
+  .set('units','imperial')
+  .set('appid',this.apiKey)
+
+  return this.http.get(this.url, { params });
+}
 }
