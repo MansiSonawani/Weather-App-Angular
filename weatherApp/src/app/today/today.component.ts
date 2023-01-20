0fa9ba4334e6356776d84f1c0ab0bdc225b613b7
Mansi Sonawani <12345mansis@gmail.com>
2023-01-20 22:54:05 +0530

    Errors with name object

--- weatherApp/src/app/today/today.component.ts
+++ weatherApp/src/app/today/today.component.ts
@@ -1,30 +1,35 @@
 import { getLocaleDirection } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { WeatherService } from '../weather.service';
+import { WeatherService} from '../weather.service';
 
 @Component({
   selector: 'app-today',
   templateUrl: './today.component.html',
   styleUrls: ['./today.component.css']
 })
-export class TodayComponent implements OnInit {
+
+export class TodayComponent  {
   lat : number | undefined;
   lon: number | undefined;
-  weather: Object | undefined;
+  name : undefined;
+  weather!: Object;
+  constructor(private weatherService : WeatherService)
+  {
 
-  constructor(private weatherService : WeatherService){}
+
+  }
    
-  ngOnInit()  {
-      this.getLocation()
+  ngOnInit(): void  {
+      this.getLocation();
+      console.warn("get your location");
 
-}
+  }
 getLocation(){
   if("geolocation" in navigator){
     navigator.geolocation.watchPosition((success) => {
       this.lat = success.coords.latitude;
       this.lon = success.coords.longitude;
-
-      this.weatherService.getWeatherDataByCoords(this.lat, this.lon).subscribe(data =>{
+      this.weatherService.getWeatherDataByCoords(this.lat, this.lon, this.name).subscribe(data =>{
         this.weather = data;
       });
 
--- weatherApp/src/app/weather.service.ts
+++ weatherApp/src/app/weather.service.ts
@@ -1,21 +1,28 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  
 url = 'https://api.openweathermap.org/data/2.5/weather';
-apiKey = '459d7ab5fab85c9b3ce557236603f3eb';
+apikey='459d7ab5fab85c9b3ce557236603f3eb';
+
    constructor(private http: HttpClient) { }
 
 
-getWeatherDataByCoords(lat: number ,lon:  number ){
+  getWeatherDataByCoords(lat: string | number | boolean,lon: string | number | boolean,name : undefined){
   
-  let params = new HttpParams()
-  .set('lat', lat)
-  .set('lon', lon)
-  .set('units','imperial')
-  .set('appid',this.apiKey)
+    let params = new HttpParams()
+      .set('lat', lat)
+      .set('lon', lon)
+      .set('name', 'name')
+      .set('units','imperial')
+      .set('api_is', this.apikey)
+  
+
+  
 
   return this.http.get(this.url, { params });
 }
