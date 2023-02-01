8e9057564f57f6452ae00fc8abad34ac6dac74e8
Mansi Sonawani <12345mansis@gmail.com>
2023-02-01 20:33:24 +0530

    Environment variables

--- weatherApp/src/environment/environment.ts
+++ weatherApp/src/environment/environment.ts
@@ -1,4 +1,9 @@
 export const environment = {
     production : false,
-    WeatherApiBaseUrl : 'https://open-weather-map27.p.rapidapi.com/weather'
+    WeatherApiBaseUrl : 'https://open-weather-map27.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderLabel : 'x-RapidAPI-Host', 
+    XRapiAPIHostHeaderValue : 'open-weather-map27p.rapidapi.com',
+    XRapidAPIKeyHeaderName : 'X-RapidAPI',
+    XRapidApiKeyValue: '4ca78aa6d4mshb107fc4a6741de5p14414cjsnb496ba7048a9'
+
 }
