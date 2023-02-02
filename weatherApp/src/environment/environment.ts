73ca4c4b54fb195920c276327c89c3339110c8a6
Mansi Sonawani <12345mansis@gmail.com>
2023-02-02 23:51:37 +0530

    git push origin main git commit -m "api errors"

--- weatherApp/src/environment/environment.ts
+++ weatherApp/src/environment/environment.ts
@@ -1,9 +1,5 @@
 export const environment = {
     production : false,
     WeatherApiBaseUrl : 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}',
-    XRapidAPIHostHeaderLabel : 'x-RapidAPI-Host', 
-    XRapiAPIHostHeaderValue : 'open-weather-map27p.rapidapi.com',
-    XRapidAPIKeyHeaderName : 'X-RapidAPI',
-    XRapidAPIKeyValue: '4ca78aa6d4mshb107fc4a6741de5p14414cjsnb496ba7048a9'
-
+ 
 }
